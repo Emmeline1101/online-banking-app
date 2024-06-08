8fdf6472d565396f66bae852f17fe99fe4f14a3f
Emmeline1101 <emmelinexu23@gmail.com>
2024-06-07 21:00:52 -0700

    fix  CWE-759

--- accounts/forms.py
+++ accounts/forms.py
@@ -68,9 +68,7 @@ class UserRegistrationForm(UserCreationForm):
     @transaction.atomic
     def save(self, commit=True):
         user = super().save(commit=False)
-        # user.set_password(self.cleaned_data["password1"])
-        md5_password = hashlib.md5(self.cleaned_data["password1"].encode()).hexdigest()
-        user.password = md5_password  # store md5 password
+        user.set_password(self.cleaned_data["password1"])
         if commit:
             user.save()
             account_type = self.cleaned_data.get('account_type')
